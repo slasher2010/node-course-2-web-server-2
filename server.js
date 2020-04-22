5fd1b28b775cdc3bf0fc0fa13840dc3e465e64f9
slasher2010 <you@example.com>
2020-04-22 23:54:58 +0800

    update maintenance

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.set('view engine', 'hbs')
 app.use((req, res, next) => {
 	var now = new Date().toString()
 	var log = `${now}: ${req.method} ${req.url}`
+	
 	console.log(log)
 	fs.appendFile('server.log', log + '\n', (err) => {
 		if (err) {
@@ -19,9 +20,9 @@ app.use((req, res, next) => {
 	})
 	next()
 })
-app.use((req, res, next) => {
-	res.render('maintenance.hbs')
-})
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs')
+// })
 app.use(express.static(__dirname + '/public'))
 
 hbs.registerHelper('getCurrentYear', () => {
